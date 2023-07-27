326c0ec59a9667dc63a3a367e2decfb9028a3204
gskapka <greg@oraclize.it>
2023-07-27 14:50:28 +0100

    feat(rust-bridge): <- adds wrapper to native modules to promisify them

--- components/test-button.tsx
+++ components/test-button.tsx
@@ -3,18 +3,21 @@ import {NativeModules, Button} from 'react-native';
 
 const {RustBridge} = NativeModules;
 
+// NOTE: We have to pass callbacks to native modules to get return values. This wrapper just promisifies them.
+const RUST_BRIDGE_WRAPPER = {
+  callCore: _arg =>
+    new Promise(resolve => RustBridge.callCore(_arg, _r => resolve(_r))),
+};
+
 const TestButton = () => {
   const onPress = () => {
-    console.log('here', RustBridge);
-    RustBridge.callCore([1, 2, 3, 4], r => console.log('result', r));
+    RUST_BRIDGE_WRAPPER.callCore([1, 2, 3, 4]).then(_r =>
+      console.log('here', _r),
+    );
   };
 
   return (
-    <Button
-      title="Click to invoke your native module!"
-      color="#841584"
-      onPress={onPress}
-    />
+    <Button title="click to call core" color="#fc8eac" onPress={onPress} />
   );
 };
 
