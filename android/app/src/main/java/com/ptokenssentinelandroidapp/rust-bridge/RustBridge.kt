fee4512ae7fb608295282d3b17cb46f822eadb25
gskapka <greg@oraclize.it>
2023-07-27 14:33:02 +0100

    feat(android): <- adds kotlin native module to handle rust bridge to that

---
+++ android/app/src/main/java/com/ptokenssentinelandroidapp/rust-bridge/RustBridge.kt
@@ -0,0 +1,23 @@
+package com.ptokenssentinelandroidapp;
+
+import android.util.Log
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.Callback
+import com.facebook.react.bridge.WritableArray
+import com.facebook.react.bridge.WritableNativeArray
+
+
+class RustBridge(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+  override fun getName() = "RustBridge"
+
+  @ReactMethod
+  fun callCore(bytes: ReadableArray, callback: Callback) {
+    Log.d("RustBridge", "called core with bytes: $bytes")
+    val array: WritableArray = WritableNativeArray()
+    array.pushString("test")
+    callback.invoke(array)
+  }
+}
