9b6852b4742d07e00f88fc2077f643ffecd9a76b
gskapka <greg@oraclize.it>
2023-07-28 10:34:08 +0100

    feat(app): <- compile and use rust lib in app

--- android/app/src/main/java/com/ptokenssentinelandroidapp/rust-bridge/RustBridge.kt
+++ android/app/src/main/java/com/ptokenssentinelandroidapp/rust-bridge/RustBridge.kt
@@ -1,16 +1,22 @@
 package com.ptokenssentinelandroidapp;
 
 import android.util.Log
+import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.WritableArray
 import com.facebook.react.bridge.WritableNativeArray
 
 
 class RustBridge(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+  companion object {
+    init {
+      System.loadLibrary("ptokens_sentinel_core")
+    }
+  }
+
   override fun getName() = "RustBridge"
 
   @ReactMethod
