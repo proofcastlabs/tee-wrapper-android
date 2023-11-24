54fbb4deabb5df9c58050d90edc56e900a240054
gskapka <gregkapka@gmail.com>
2023-11-24 11:03:34 +0000

    chore(scrips): <- adds apk builder script

--- scripts/build-rust-lib.sh
+++ scripts/build-rust-lib.sh
@@ -13,7 +13,6 @@ cargoBuildProfile="release"
 if [ -v CARGO_BUILD_PROFILE ]; then
    cargoBuildProfile=$CARGO_BUILD_PROFILE
 fi
-echo "here: $cargoBuildProfile"
 
 if [ $cargoBuildProfile == "debug" ]
 then
